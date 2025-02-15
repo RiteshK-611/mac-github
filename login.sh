d4beb94ba7a50c4a432aea79f7f8a8120649dd73
Ritesh Kokam <61982298+RiteshK-611@users.noreply.github.com>
2025-02-16 03:15:26 +0530

    Create login.sh

---
+++ login.sh
@@ -0,0 +1,8 @@
+# login.sh
+#!/bin/bash
+echo ".........................................................."
+echo "VNC Connection Details:"
+echo "Address: $BORE_URL"
+echo "Username: runneradmin"
+echo "Password: P@ssw0rd!"
+echo ".........................................................."
